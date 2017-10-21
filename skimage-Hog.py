03c91458a1a66af7bdbb79b2ad8cd89a9177a2f7
maged.rawash <maged@rawash.com>
2017-10-22 01:36:03 +0200

    solve issue then memory error

--- skimage-Hog.py
+++ skimage-Hog.py
@@ -1,14 +1,43 @@
+"""
 
+car_file Loaded 337780
+notcar_file Loaded  358071
+337780
+car_file Loaded 337780
+notcar_file Loaded  337780
+X.shape (675560, 1760)
+Traceback (most recent call last):
+  File "C:\Program Files\JetBrains\PyCharm Community Edition 2017.2.3\helpers\pydev\pydevd.py", line 1599, in <module>
+    globals = debugger.run(setup['file'], None, None, is_module)
+  File "C:\Program Files\JetBrains\PyCharm Community Edition 2017.2.3\helpers\pydev\pydevd.py", line 1026, in run
+    pydev_imports.execfile(file, globals, locals)  # execute the script
+  File "C:\Program Files\JetBrains\PyCharm Community Edition 2017.2.3\helpers\pydev\_pydev_imps\_pydev_execfile.py", line 18, in execfile
+    exec(compile(contents+"\n", file, 'exec'), glob, loc)
+  File "D:/Privet/work/Python/PycharmProjects/parking-car-counter/skimage-Hog.py", line 72, in <module>
+    X_scaler = StandardScaler().fit(X)
+  File "C:\Program Files\Anaconda3\lib\site-packages\sklearn\preprocessing\data.py", line 557, in fit
+    return self.partial_fit(X, y)
+  File "C:\Program Files\Anaconda3\lib\site-packages\sklearn\preprocessing\data.py", line 621, in partial_fit
+    self.n_samples_seen_)
+  File "C:\Program Files\Anaconda3\lib\site-packages\sklearn\utils\extmath.py", line 742, in _incremental_mean_and_var
+    new_unnormalized_variance = X.var(axis=0) * new_sample_count
+  File "C:\Program Files\Anaconda3\lib\site-packages\numpy\core\_methods.py", line 101, in _var
+    x = asanyarray(arr - arrmean)
+MemoryError
+
+
+"""
 import glob
 import numpy as np 
-from skimage.feature import hog
-from sklearn.preprocessing import StandardScaler
-from skimage import color, exposure, io
-
+from sklearn.svm import LinearSVC
+from sklearn.cross_validation import train_test_split
+import pickle
 # from dataHandling import dataBase 
+# Positive, Negative = dataBase()
 
-# Positive, Negative = dataBase() 
- 
+# from skimage import color, exposure, io
+# from skimage.feature import hog
+
 # def extract_feature(images):
 #     features = np.array([])
 #     for image in images:
@@ -35,25 +64,45 @@ print("car_file Loaded" , len(car_features ) )
 notcar_features =  np.load("notcar_features.p")
 print("notcar_file Loaded ", len(notcar_features) )
 
-# minNump = np.min(np.array([car_features.size, notcar_features.size]))
-# print(minNump)
-# # car_features = car_features
-# print("car_file Loaded" , len(car_features ))
-# notcar_features = notcar_features
-# print("notcar_file Loaded ", len(notcar_features) )
+minNump = np.min(np.array([len(car_features), len(notcar_features)]))
+print(minNump)
+car_features = car_features[:minNump]
+print("car_file Loaded" , len(car_features ))
+notcar_features = notcar_features[:minNump]
+print("notcar_file Loaded ", len(notcar_features) )
 
 ### 
 
+length = len(sorted(car_features,key=len, reverse=True)[0])
+car_features= np.array([np.hstack((xi , [0]*(length-len(xi)))) for xi in car_features])
+
+pickle.dump(car_features, open("car_features_generalisedList.p", 'wb'), protocol=4)
+
+notlength = len(sorted(notcar_features,key=len, reverse=True)[0])
+notcar_features= np.array([np.hstack((xi , [0]*(notlength-len(xi)))) for xi in notcar_features])
+
+pickle.dump(notcar_features, open("notcar_features_generalisedList.p", 'wb'), protocol=4)
+
+X = np.vstack((car_features, notcar_features)).astype(np.float64)
+
 # Create an array stack of feature vectors
-X = np.concatenate( ( car_features, notcar_features ) )
+# X = np.concatenate( ( car_features, notcar_features ) )
+# print( 'X.shape', X.shape )
+# X = np.array([car_features, notcar_features])
+
+# X = np.vstack(list(X[:,:]))
+# X = np.asfarray(X)
 print( 'X.shape', X.shape )
 
-# Fit a per-column scaler 
+from sklearn.preprocessing import StandardScaler
+
+# Fit a per-column scaler
 X_scaler = StandardScaler().fit(X)
 # Apply the scaler to X
 scaled_X = X_scaler.transform(X)
 
-np.asarray(X_scaler).dump("saved_X_scaler.dat")
+print("now scaled_X step")
+np.asarray(scaled_X).dump("saved_X_scaler.dat")
 
 # Define the labels vector
 y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
--- test.py
+++ test.py
@@ -1,32 +1,30 @@
-
 import glob
-import numpy as np 
+import numpy as np
+
+car_features = np.load("car_features.p")[:10000]
+print("car_file Loaded", len(car_features))
+
+notcar_features = np.load("notcar_features.p")[:10000]
+print("notcar_file Loaded ", len(notcar_features))
 
 
-###
 
-# car_features =np.asarray( np.load("car_features.p") )
-# print("car_file Loaded" , car_features.shape )
-
-# notcar_features = np.asarray( np.load("notcar_features.p"))
-# print("notcar_file Loaded ", notcar_features.shape )
-
-# car_features.dump("car_features_RGB_nparr.p")
-
-# notcar_features.dump("notcar_features_RGB_nparr.p")
-
-### 
-
-x = np.array([1,2,3,4,5,6,55,8,33,34,85])
-y = np.array([7,8,9,10,11])
+#
+# car_features = np.asarray(notcar_features, dtype=np.float64)
+# notcar_features = np.asarray(notcar_features, dtype=np.float64)
 
 
-t1 = np.stack((x,y))
-t2 = np.asarray((x,y))
 
-X = np.concatenate( (x,y) )
-    
-print('X.shape',X.shape)
-print(t1)
-print(t2)
-print(X)
+
+print('X.shape', X.shape)
+
+from sklearn.preprocessing import StandardScaler
+
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+print("now scaled_X step")
+scaled_X.dump("scaled_X.p")
+
